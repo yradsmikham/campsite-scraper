0251c29d63b1c67de8e34d0d97a467f831f3675e
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-24 16:41:29 -0800

    first commit-- prototype

---
+++ webscrapy.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from textmagic.rest import TextmagicRestClient
+import requests, json, urllib2
+
+client = TextmagicRestClient("yradsmikham", "VlY7PjdtNSZkiiyZn6U1GX0KV7jLTl")
+
+# inputs
+# enter phone number, specify dates and campground id
+phone_number = "" # must include country code
+campground_id = "232447" # example correponds to Yosemite Upper Pines Campground
+start_date = "2019-06-14"
+end_date = "2019-06-16"
+
+# specify url
+url = "https://www.recreation.gov/api/camps/availability/campground/"+campground_id+"?start_date="+start_date+"T00%3A00%3A00.000Z&end_date="+end_date+"T00%3A00%3A00.000Z"
+
+# query website and return html
+data = urllib2.urlopen(url)
+
+# parse the html using beautiful soup and store in variable 
+soup = str(BeautifulSoup(data, 'html.parser'))
+campsite_json = json.loads(soup)
+
+# iterate through each campsite to check for availabilities
+available_campsites = []
+for campsite in campsite_json['campsites']:
+    days = campsite_json['campsites'][campsite]["availabilities"]
+    availabilities = []
+    for key,value in days.items():
+        if value != None:
+            availabilities.append(value)
+    if not availabilities:
+        print("List is empty.")
+    else:
+        print(availabilities)
+        print(all(dawgie == 'Open' for dawgie in availabilities))
+        if all(dawgie == 'Open' and not None for dawgie in availabilities):
+            print("THERE IS AN AVAILABLE CAMPSITE!")
+            available_campsites.append(campsite)
+        else:
+            print("NO CAMPSITES AVAILABLE.")
+    print("------------------------------------------------------------------------")
+print("AVAILABLE CAMPSITES:" + str(available_campsites))
+
+# send text notification
+client.messages.create(phones=phone_number, text="NO CAMPSITES!")
+
+# notes:
+# multiprocessing/threaded?
+# possibly kick off another script that will log in and adds to cart?
